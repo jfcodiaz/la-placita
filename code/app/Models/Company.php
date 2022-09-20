2400373bd0de9f9b0f71dafc793c5413011415ba
Julio Lira <jlira@estrasol.com.mx>
2022-09-20 17:20:52 -0500

    Add Company Model

---
+++ code/app/Models/Company.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $type_id
+ * @property string $name
+ * @property Collection $branches
+ * @property DateTime $created_at
+ * @property DateTime $updated_at
+ */
+class Company extends Model
+{
+    use HasFactory;
+}
